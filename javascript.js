fa6be6371c9dbff95a9fab8043ea9344cee32a5d
YoshiGuy2000 <danielavilalearns@gmail.com>
2023-12-29 18:40:22 -0500

    Completed rock paper scissors code

--- javascript.js
+++ javascript.js
@@ -2,46 +2,59 @@ function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"]
     let choice = Math.floor(Math.random() * choices.length)
     return choices[choice]
+
 }
 
-function oneRound(playerSelection, computerSelection) {
+function oneRound(playerSelection) {
+    let computerSelection = getComputerChoice()
     let playerLower = playerSelection.toLowerCase()
     let computerLower = computerSelection.toLowerCase()
-
+    let game_text = document.getElementById("results")
 
     if (playerLower == computerLower) {
-        console.log("A tie! Play again!")
+        game_text.textContent = "A tie! Play again!"
+        //console.log("A tie! Play again!")
         /*oneRound(playerSelection, computerSelection)*/
     }
 
     else if (playerLower == "rock" && computerLower == "paper") {
-        console.log("You lose! Paper beats rock!")
+        game_text.textContent = "You lose! Paper beats rock!"
+        //console.log("You lose! Paper beats rock!")
         return "computer_win"
     }
 
     else if (playerLower == "rock" && computerLower == "scissors") {
-        console.log("You win! Rock beats scissors!")
+        game_text.textContent = "You win! Rock beat scissors!"
+        //console.log("You win! Rock beats scissors!")
         return "player_win"
     }
 
     else if (playerLower == "paper" && computerLower == "rock") {
-        console.log("You win! Paper beats rock!")
+        game_text.textContent = "You win! Paper beats rock!"
+        //console.log("You win! Paper beats rock!")
         return "player_win"
     }
 
     else if (playerLower == "paper" && computerLower == "scissors") {
-        console.log("You lose! Scissors beats paper!")
+        game_text.textContent = "You win! Scissors beats paper!"
+        //console.log("You lose! Scissors beats paper!")
         return "computer_win"
     }
 
     else if (playerLower == "scissors" && computerLower == "paper") {
-        console.log("You win! Scissors beats paper!")
+        game_text.textContent = "You win! Scissors bears paper!"
+        //console.log("You win! Scissors beats paper!")
         return "player_win"
     }
 
     else if (playerLower == "scissors" && computerLower == "rock") {
-        console.log("You lose! Scissors beats rock!")
+        game_text.textContent = "You lose! Scissors beats rock!"
+        //console.log("You lose! Scissors beats rock!")
         return "computer_win"
+    }
+
+    else {
+        console.log("Button works, no response")
     }
 
 }
@@ -51,9 +64,13 @@ function game() {
     player_score = 0
 
     while (computer_score < 5 && player_score < 5) {
+        let score_text = document.getElementById("score")
+        score_text.textContent = ("Player: ${player_score} Computer: ${computer_score}")
         console.log(computer_score, player_score)
-        let player_guess = prompt("Rock, paper, or scissors: ")
-        let outcome = oneRound(player_guess, getComputerChoice())
+        //let player_guess = prompt("Rock, paper, or scissors: ")
+        //let outcome = oneRound(player_guess, getComputerChoice())
+
+        let outcome = button_click
         if (outcome == "player_win") {
             player_score = player_score + 1
         }
@@ -72,5 +89,15 @@ function game() {
     }
 }
 
+function test() {
+    console.log("This button works!")
+}
+
+function button_click() {
+    outcome = document.getElementById("rock").addEventListener("click", () => { oneRound("Rock") })
+    outcome = document.getElementById("paper").addEventListener("click", () => { oneRound("Paper") })
+    outcome = document.getElementById("scissors").addEventListener("click", () => { oneRound("Scissors") })
+    return outcome
+}
 
 game()
